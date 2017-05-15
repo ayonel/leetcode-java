f2dec48acd755cd6aec3653c9217b20096c2da85
ayonel <ayonel@163.com>
2017-05-15 22:33:09 +0800

    daily

---
+++ src/ayonel_27_RemoveElement/Solution.java
@@ -0,0 +1,26 @@
+package ayonel_27_RemoveElement;
+
+/**
+ * Remove Element
+ *
+ * @author ayonel
+ * @create 2017-05-15 22:17
+ * @blog https://ayonel.me
+ **/
+public class Solution {
+    public  int removeElement(int[] nums, int val) {
+        int id = 0;
+        for (int i = 0; i < nums.length ; i++) {
+            if (nums[i] != val) {
+                nums[id++] = nums[i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            System.out.println(nums[i]);
+        }
+        return id;
+
+    }
+
+
+}
