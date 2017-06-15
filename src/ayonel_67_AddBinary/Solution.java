468adcb75d8252cd28660b995c6b06ad0f4848be
ayonel <ayonel@163.com>
2017-06-15 20:36:46 +0800

    text justification

---
+++ src/ayonel_67_AddBinary/Solution.java
@@ -0,0 +1,71 @@
+package ayonel_67_AddBinary;
+
+/**
+ * 67. Add Binary
+ *
+ * @author ayonel
+ * @create 2017-06-15 16:01
+ * @blog https://ayonel.me
+ **/
+public class Solution {
+    public String addBinary(String a, String b) {
+        int aLen = a.length();
+        int bLen = b.length();
+        if (aLen == 0)
+            return b;
+        if (bLen == 0)
+            return a;
+
+        if (aLen > bLen)
+            return solve(a, b);
+        else
+            return solve(b, a);
+
+    }
+
+    public String solve(String a, String b) {
+        // a是长度更长的
+        int aLen = a.length();
+        int bLen = b.length();
+
+        int carry = 0;
+        int value = 0;
+        StringBuilder str = new StringBuilder();
+        int i = 0;
+        for (; i < bLen; i++) {
+            value = carry + (a.charAt(aLen-1-i)-'0') + (b.charAt(bLen-1-i)-'0');
+            carry = value / 2;
+            value = value % 2;
+            str.insert(0, value);
+        }
+        for(int j = i; j < aLen; j++) {
+            value = carry + (a.charAt(aLen-1-j)-'0');
+            carry = value / 2;
+            value = value % 2;
+            str.insert(0, value);
+        }
+
+        if (carry == 1){
+            str.insert(0,'1');
+        }
+
+        return str.toString();
+    }
+
+    public String addBinaryStandard(String a, String b) {
+        StringBuilder sb = new StringBuilder();
+        int i = a.length() - 1, j = b.length() -1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (j >= 0) sum += b.charAt(j--) - '0';
+            if (i >= 0) sum += a.charAt(i--) - '0';
+            sb.append(sum % 2);
+            carry = sum / 2;
+        }
+        if (carry != 0) sb.append(carry);
+        return sb.reverse().toString();
+    }
+    public static void main(String[] args) {
+        System.out.println(new Solution().addBinary("1011","1"));
+    }
+}
---
+++ src/ayonel_68_TextJustification/Solution.java
@@ -0,0 +1,91 @@
+package ayonel_68_TextJustification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author ayonel
+ * @create 2017-06-15 16:45
+ * @blog https://ayonel.me
+ **/
+public class Solution {
+    public List<String> fullJustify(String[] words, int maxWidth) {
+        List<String> res = new ArrayList<>();
+        int curLengthOfWord = 0;
+        List<String> str = new ArrayList<String>();
+        if (words.length == 0)
+            return res;
+        for (int i = 0; i < words.length - 1; i++) {
+            curLengthOfWord += words[i].length();
+            str.add(words[i]);
+            if (curLengthOfWord + words[i+1].length() + str.size() > maxWidth){
+                StringBuilder curStr = new StringBuilder(str.get(0));
+                str.remove(0);
+                int spaceNum = maxWidth - curLengthOfWord;
+                int k = 0;
+                if (spaceNum == 0){
+                    res.add(curStr.toString());
+                    curLengthOfWord = 0;
+                    str = new ArrayList<>();
+                    continue;
+                }
+                if (str.size() == 0) {
+                    while(k < spaceNum) {
+                        curStr.append(' ');
+                        k++;
+                    }
+                    res.add(curStr.toString());
+                    curLengthOfWord = 0;
+                    str = new ArrayList<>();
+                    continue;
+                }
+
+                ArrayList<Integer> spaceNumList = new ArrayList<>();
+                for (int j = 0; j < str.size(); j++) {
+                    spaceNumList.add(0);
+                }
+                while(k < spaceNum) {
+                    spaceNumList.set(k%str.size(), spaceNumList.get(k%str.size())+1);
+                    k++;
+                }
+
+                for (int j = 0; j < str.size(); j++) {
+                    for (int l = 0; l < spaceNumList.get(j); l++) {
+                        curStr.append(" ");
+                    }
+                    curStr.append(str.get(j));
+                }
+                res.add(curStr.toString());
+                curLengthOfWord = 0;
+                str = new ArrayList<>();
+            }
+
+        }
+        str.add(words[words.length-1]);
+        StringBuilder curStr = new StringBuilder(str.get(0));
+        str.remove(0);
+        if (curStr.length() == maxWidth) {
+            res.add(curStr.toString());
+            return res;
+        }
+
+        for (String lastLineEle: str) {
+            if (curStr.length()+lastLineEle.length() + 1 <= maxWidth)
+                curStr.append(" "+lastLineEle);
+        }
+        for (int i = curStr.length(); i < maxWidth; i++) {
+            curStr.append(' ');
+        }
+
+        res.add(curStr.toString());
+
+        return res;
+
+    }
+
+    public static void main(String[] args) {
+        String[] words = {"world","owes","you","a","living;","the","world"};
+//        String[] words = {""};
+        System.out.println(new Solution().fullJustify(words, 30));
+    }
+}
